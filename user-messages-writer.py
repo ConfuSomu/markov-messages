9b980b0100ed5d5b7a94d985fe6bd1d4219df395
ConfuSomu <emilemeekel+github@gmail.com>
2022-03-03 14:52:17 -0500

    Clean-up user-messages-writer.py

--- user-messages-writer.py
+++ user-messages-writer.py
@@ -1,17 +1,12 @@
 import json
 import re
 
-def is_discord_messages(messages):
+def for_discord_messages(messages):
     messages_content = []
-
-   # escapes_pattern = re.compile(r'\Z', flags=re.IGNORECASE) # This should match the newline escapes (\n) and others, except \uXXXX for unicode
 
     for message in messages:
         if message[0] != "ID": # This is a CSV header, ignore it then...
             text = message[2]
-            # matches = escapes_pattern.findall(text)
-            # if matches:
-            #    pass
             
             messages_content.append(text)
     
@@ -26,7 +21,7 @@ def parse_messages_json(messages_json_file, messages_text_file):
 
     messages_text = []
     if contents["message-source"] == "discord":
-        messages_text = is_discord_messages(contents["messages"])
+        messages_text = for_discord_messages(contents["messages"])
     
     with open(messages_text_file, 'a') as file:
         for line in messages_text:
